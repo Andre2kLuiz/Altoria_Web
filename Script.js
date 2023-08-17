664ec604f9acebf481bfe722dd3e3366057a6922
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-08-17 10:46:51 -0300

    up

--- Script.js
+++ Script.js
@@ -1,5 +1,29 @@
 function alteraTexto() {
-    document.querySelector("btn").textContent = "Ola Mundo"
+    document.getElementById("header").innerText = "Ola Mundo"
 }
+
+
+function soma (a, b){
+    return a + b
+}
+
+let aula = soma
+
+console.log(typeof soma)
+console.log(typeof aula) 
+
+soma = 7
+
+console.log(typeof soma)
+
+
+const subtração = (a, b) => {
+    return a - b
+}
+
+console.log(`subtração tipo = ${typeof subtração}`)
+
+console.log(subtração(10, 7))
+console.log(aula(10, 7))
 
 
