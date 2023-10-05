35038037f8226e4260031dea4f7013006065dc8d
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-10-05 09:54:53 -0300

    up

--- AulaZeroJs/Script.js
+++ AulaZeroJs/Script.js
@@ -1,1 +1,14 @@
-console.log("Hello Word!");
+let ent = document.querySelector('#entrada')
+let btn = document.querySelector('#btn')
+let rep = document.querySelector('#resposta')
+let form = document.querySelector('#form')
+const valorDolar = 5.06
+
+rep.addEventListener("click", function(event){
+    event.preventDefault
+
+    rep.classList.add
+})
+
+
+
