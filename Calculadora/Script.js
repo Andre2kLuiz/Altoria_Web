a8499f07ecd0bd3dc4c2f2294649bde62844731f
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-10-14 08:10:09 -0300

    atualização

--- Calculadora/Script.js
+++ Calculadora/Script.js
@@ -1,9 +1,14 @@
 
 let flag = true
+let cont = 0
 
 /*Função para escrever na tela os números, concatenalos e liberar flag para operadores */
 
 function inserir (num){
+    if(cont === 0){
+        document.getElementById('resultado').innerHTML = ""
+        cont++
+    }
     let numero = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = numero + num
     flag = true
@@ -22,7 +27,8 @@ function inserirOp (op){
 /*Limpar a tela */
 
 function limpar (){
-    document.getElementById('resultado').innerHTML = ""
+    document.getElementById('resultado').innerHTML = 0
+    cont = 0
 }
 
 /*Apagar numero por numero da tela */
@@ -30,6 +36,10 @@ function limpar (){
 function apagar (){
     let resultado = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+    if(resultado.length === 1){
+        document.getElementById('resultado').innerHTML = 0
+        cont = 0
+    }
 }
 
 /*função de caulculo dos numeros usando eval. calculando em string sem usar o typecast */
