99cd6ed5254412fb78b73d1e0db8427a65b61a42
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-10-14 07:36:32 -0300

    comentarios

--- Calculadora/Script.js
+++ Calculadora/Script.js
@@ -1,12 +1,15 @@
 
 let flag = true
 
+/*Função para escrever na tela os números, concatenalos e liberar flag para operadores */
 
 function inserir (num){
     let numero = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = numero + num
     flag = true
 }
+
+/*Função para escrever operadores na tela e bloquear repetição seguidas de operadores */
 
 function inserirOp (op){
     if(flag === true){
@@ -16,14 +19,20 @@ function inserirOp (op){
     }
 }
 
+/*Limpar a tela */
+
 function limpar (){
     document.getElementById('resultado').innerHTML = ""
 }
+
+/*Apagar numero por numero da tela */
 
 function apagar (){
     let resultado = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
 }
+
+/*função de caulculo dos numeros usando eval. calculando em string sem usar o typecast */
 
 function calcular(){
     let resultado = document.getElementById('resultado').innerHTML
